9ffe11887a02d314b3bf4136b79c267786ea8c9f
Arun Selvan <53175578+gjarunselvan@users.noreply.github.com>
2026-06-17 16:38:17 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,8 @@
 sudo docker inspect jenkins | grep docker.sock
-
-sudo docker exec -it jenkins sh -c "docker --version"
+                "/var/run/docker.sock:/var/run/docker.sock"
+                "Source": "/var/run/docker.sock",
+                "Destination": "/var/run/docker.sock",
+ubuntu@ip-172-31-39-221:~$ sudo docker exec -it jenkins sh -c "docker --version"
+sh: 1: docker: not found
+ubuntu@ip-172-31-39-221:~$ ^C
+ubuntu@ip-172-31-39-221:~$ 
