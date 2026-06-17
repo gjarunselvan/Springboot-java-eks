e77df820fa20ab28fae70e5e3ae312254fabaa4d
Arun Selvan <53175578+gjarunselvan@users.noreply.github.com>
2026-06-17 16:48:56 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,7 @@
-./setup.sh 
-Docker Socket GID: 986
-jenkins
-Waiting for Jenkins...
-Jenkins User:
-uid=1000(jenkins) gid=1000(jenkins) groups=1000(jenkins),986(dockerhost)
-Testing Docker Access:
-CONTAINER ID   IMAGE                 COMMAND                  CREATED         STATUS          PORTS                                                                                      NAMES
-a06178967cf0   jenkins/jenkins:lts   "/usr/bin/tini -- /u…"   9 minutes ago   Up 20 seconds   0.0.0.0:8080->8080/tcp, :::8080->8080/tcp, 0.0.0.0:50000->50000/tcp, :::50000->50000/tcp   jenkins
-ubuntu@ip-172-31-39-221:~$ ^C
+sudo docker volume ls
+DRIVER    VOLUME NAME
+local     17ab0b3182e2c6111feeb1b82f80d89cc7812802825dfe7f1bb2862a4d2aeaa0
+local     jenkins_data
+local     jenkins_home
+local     jenkins_network
 ubuntu@ip-172-31-39-221:~$ 
