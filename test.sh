40786290e726bf41e0b8813843e764684e31dbed
Arun Selvan <53175578+gjarunselvan@users.noreply.github.com>
2026-06-17 22:11:37 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,18 @@
-sudo docker exec jenkins docker --version
-Docker version 26.1.5+dfsg1, build a72d7cd
-ubuntu@ip-172-31-39-221:~$ sudo docker exec jenkins docker ps
-permission denied while trying to connect to the Docker daemon socket at unix:///var/run/docker.sock: Get "http://%2Fvar%2Frun%2Fdocker.sock/v1.45/containers/json": dial unix /var/run/docker.sock: connect: permission denied
-ubuntu@ip-172-31-39-221:~$ 
+# Get Docker group ID from host
+DOCKER_GID=$(getent group docker | cut -d: -f3)
+
+echo "Docker GID: $DOCKER_GID"
+
+# Add Jenkins user inside container to host Docker group
+sudo docker exec -u root jenkins groupadd -g $DOCKER_GID docker-host 2>/dev/null || true
+
+sudo docker exec -u root jenkins usermod -aG $DOCKER_GID jenkins
+
+# Restart container
+sudo docker restart jenkins
+
+# Wait a few seconds
+sleep 15
+
+# Test Docker access
+sudo docker exec jenkins docker ps
