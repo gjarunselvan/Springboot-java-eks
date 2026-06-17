ed392ffcb086b63e98344bd029709a921a75e142
Arun Selvan <53175578+gjarunselvan@users.noreply.github.com>
2026-06-17 16:47:35 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,18 +1,11 @@
-DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
-
-echo "Docker Socket GID: $DOCKER_GID"
-
-sudo docker exec -u root jenkins groupadd -g $DOCKER_GID dockerhost 2>/dev/null || true
-
-sudo docker exec -u root jenkins usermod -aG dockerhost jenkins
-
-sudo docker restart jenkins
-
-echo "Waiting for Jenkins..."
-sleep 20
-
-echo "Jenkins User:"
-sudo docker exec jenkins id
-
-echo "Testing Docker Access:"
-sudo docker exec jenkins docker ps
+./setup.sh 
+Docker Socket GID: 986
+jenkins
+Waiting for Jenkins...
+Jenkins User:
+uid=1000(jenkins) gid=1000(jenkins) groups=1000(jenkins),986(dockerhost)
+Testing Docker Access:
+CONTAINER ID   IMAGE                 COMMAND                  CREATED         STATUS          PORTS                                                                                      NAMES
+a06178967cf0   jenkins/jenkins:lts   "/usr/bin/tini -- /u…"   9 minutes ago   Up 20 seconds   0.0.0.0:8080->8080/tcp, :::8080->8080/tcp, 0.0.0.0:50000->50000/tcp, :::50000->50000/tcp   jenkins
+ubuntu@ip-172-31-39-221:~$ ^C
+ubuntu@ip-172-31-39-221:~$ 
