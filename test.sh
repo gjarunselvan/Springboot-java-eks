b0f33edb6d0d3ee0345fa006b59bc75ccfa1084f
Arun Selvan <53175578+gjarunselvan@users.noreply.github.com>
2026-06-17 16:51:25 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,24 +1,24 @@
-# Stop current Jenkins
-sudo docker stop jenkins
-sudo docker rm jenkins
-
-# Start Jenkins using the original volume
-sudo docker run -d \
---name jenkins \
---restart unless-stopped \
--p 8080:8080 \
--p 50000:50000 \
--v jenkins_data:/var/jenkins_home \
--v /var/run/docker.sock:/var/run/docker.sock \
---network jenkins_network \
-jenkins/jenkins:lts
-
-# Wait for startup
-echo "Waiting 30 seconds for Jenkins..."
-sleep 30
-
-# Verify container
-sudo docker ps
-
-# Verify mounted volume
-sudo docker inspect jenkins | grep -A3 -B3 jenkins_data
+./setup.sh 
+jenkins
+jenkins
+58119f48198ea6150edb98d7993f3764775ca0a4f22a5eed1c456a6d244eb6dc
+Waiting 30 seconds for Jenkins...
+CONTAINER ID   IMAGE                 COMMAND                  CREATED          STATUS          PORTS                                                                                          NAMES
+58119f48198e   jenkins/jenkins:lts   "/usr/bin/tini -- /u…"   30 seconds ago   Up 30 seconds   0.0.0.0:8080->8080/tcp, [::]:8080->8080/tcp, 0.0.0.0:50000->50000/tcp, [::]:50000->50000/tcp   jenkins
+        "HostConfig": {
+            "Binds": [
+                "/var/run/docker.sock:/var/run/docker.sock",
+                "jenkins_data:/var/jenkins_home"
+            ],
+            "ContainerIDFile": "",
+            "LogConfig": {
+--
+            },
+            {
+                "Type": "volume",
+                "Name": "jenkins_data",
+                "Source": "/var/lib/docker/volumes/jenkins_data/_data",
+                "Destination": "/var/jenkins_home",
+                "Driver": "local",
+                "Mode": "z",
+ubuntu@ip-172-31-39-221:~$ 
