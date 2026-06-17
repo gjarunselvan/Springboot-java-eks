2fb476112f4ae589003b236c2b965286984b9832
Arun Selvan <53175578+gjarunselvan@users.noreply.github.com>
2026-06-17 22:07:33 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,3 @@
-sudo docker stop jenkins
-sudo docker rm jenkins 
+sudo docker inspect jenkins | grep docker.sock
 
-sudo docker run -d \
---name jenkins \
---restart unless-stopped \
--p 8080:8080 \
--p 50000:50000 \
--v jenkins_home:/var/jenkins_home \
--v /var/run/docker.sock:/var/run/docker.sock \
---network jenkins_network \
-jenkins/jenkins:lts
+sudo docker exec -it jenkins sh -c "docker --version"
