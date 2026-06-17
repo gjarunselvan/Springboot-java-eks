1f6c7878963054a839907f17bfe094ef90d8869d
Arun Selvan <53175578+gjarunselvan@users.noreply.github.com>
2026-06-17 22:09:42 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,12 @@
-sudo docker inspect jenkins | grep docker.sock
-                "/var/run/docker.sock:/var/run/docker.sock"
-                "Source": "/var/run/docker.sock",
-                "Destination": "/var/run/docker.sock",
-ubuntu@ip-172-31-39-221:~$ sudo docker exec -it jenkins sh -c "docker --version"
-sh: 1: docker: not found
-ubuntu@ip-172-31-39-221:~$ ^C
-ubuntu@ip-172-31-39-221:~$ 
+# Install Docker CLI inside Jenkins container
+
+sudo docker exec -u root jenkins bash -c '
+apt-get update &&
+apt-get install -y docker.io
+'
+
+# Verify Docker is available inside Jenkins
+sudo docker exec jenkins docker --version
+
+# Verify Jenkins can talk to host Docker
+sudo docker exec jenkins docker ps
