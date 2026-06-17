1f707bb53a7f1eee9a63281c8f36046f6911798f
Arun Selvan <53175578+gjarunselvan@users.noreply.github.com>
2026-06-17 21:12:30 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,16 +1,3 @@
-echo "===== AWS CLI ====="
-aws --version
-
-echo "===== Caller Identity ====="
-aws sts get-caller-identity
-
-echo "===== Current Context ====="
-kubectl config current-context
-
-echo "===== Kubeconfig Command ====="
-grep -A5 command ~/.kube/config
-
-echo "===== EKS Token ====="
-aws eks get-token \
-  --region us-east-2 \
-  --cluster-name jenkins-cluster | head
+aws ecr describe-repositories \
+--repository-names springboot-jenkins-ecr-repo \
+--region us-east-2
