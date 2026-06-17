88ecc6954ce54b4b9e2bdd4abf1a3e59345fb8c5
Arun Selvan <53175578+gjarunselvan@users.noreply.github.com>
2026-06-17 16:41:04 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,5 @@
-# Install Docker CLI inside Jenkins container
-
-sudo docker exec -u root jenkins bash -c '
-apt-get update &&
-apt-get install -y docker.io
-'
-
-# Verify Docker is available inside Jenkins
 sudo docker exec jenkins docker --version
-
-# Verify Jenkins can talk to host Docker
-sudo docker exec jenkins docker ps
+Docker version 26.1.5+dfsg1, build a72d7cd
+ubuntu@ip-172-31-39-221:~$ sudo docker exec jenkins docker ps
+permission denied while trying to connect to the Docker daemon socket at unix:///var/run/docker.sock: Get "http://%2Fvar%2Frun%2Fdocker.sock/v1.45/containers/json": dial unix /var/run/docker.sock: connect: permission denied
+ubuntu@ip-172-31-39-221:~$ 
