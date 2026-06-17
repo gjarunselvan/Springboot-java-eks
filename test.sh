9e9b32aeda622567237acfea76244c3e71f71e01
Arun Selvan <53175578+gjarunselvan@users.noreply.github.com>
2026-06-17 22:22:15 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,24 +1,12 @@
-./setup.sh 
-jenkins
-jenkins
-58119f48198ea6150edb98d7993f3764775ca0a4f22a5eed1c456a6d244eb6dc
-Waiting 30 seconds for Jenkins...
-CONTAINER ID   IMAGE                 COMMAND                  CREATED          STATUS          PORTS                                                                                          NAMES
-58119f48198e   jenkins/jenkins:lts   "/usr/bin/tini -- /u…"   30 seconds ago   Up 30 seconds   0.0.0.0:8080->8080/tcp, [::]:8080->8080/tcp, 0.0.0.0:50000->50000/tcp, [::]:50000->50000/tcp   jenkins
-        "HostConfig": {
-            "Binds": [
-                "/var/run/docker.sock:/var/run/docker.sock",
-                "jenkins_data:/var/jenkins_home"
-            ],
-            "ContainerIDFile": "",
-            "LogConfig": {
---
-            },
-            {
-                "Type": "volume",
-                "Name": "jenkins_data",
-                "Source": "/var/lib/docker/volumes/jenkins_data/_data",
-                "Destination": "/var/jenkins_home",
-                "Driver": "local",
-                "Mode": "z",
-ubuntu@ip-172-31-39-221:~$ 
+# Check whether the long random volume contains old Jenkins data
+
+sudo docker run --rm \
+-v 17ab0b3182e2c6111feeb1b82f80d89cc7812802825dfe7f1bb2862a4d2aeaa0:/data \
+ubuntu bash -c "ls -la /data"
+
+echo ""
+echo "=== Looking for Jenkins folders ==="
+
+sudo docker run --rm \
+-v 17ab0b3182e2c6111feeb1b82f80d89cc7812802825dfe7f1bb2862a4d2aeaa0:/data \
+ubuntu bash -c "ls -la /data/jobs /data/plugins /data/users 2>/dev/null"
