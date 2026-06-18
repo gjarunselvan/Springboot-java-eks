0c285f475608a115c5d7bf4fadf48aec261c808f
Arun Selvan <53175578+gjarunselvan@users.noreply.github.com>
2026-06-18 07:32:43 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,35 +1,16 @@
-Run these commands now
-1. Verify cluster
-kubectl get nodes
-
-Expected:
-
-NAME       STATUS   ROLES           AGE   VERSION
-minikube   Ready    control-plane   ...
-2. Create Docker registry secret
-kubectl create secret docker-registry my-registry-secret \
---docker-username=gjarunselvan \
---docker-password='YOUR_DOCKER_PASSWORD'
-3. Verify secret
-kubectl get secret
-
-You must see:
-
-my-registry-secret
-4. Deploy
-
-From project folder:
-
-kubectl apply -f mysql-deployment.yaml
-kubectl apply -f todo-application-deployment.yaml
-5. Check pods
-kubectl get pods
-6. Check services
-kubectl get svc
-Send me ONLY these outputs
 kubectl get nodes
 kubectl get secret
 kubectl get pods
 kubectl get svc
-
-Once I see those 4 outputs, I'll tell you exactly what remains for the grader. You're very close to the finish line now. 🚀
+NAME       STATUS   ROLES           AGE     VERSION
+minikube   Ready    control-plane   4m56s   v1.32.0
+NAME                 TYPE                             DATA   AGE
+my-registry-secret   kubernetes.io/dockerconfigjson   1      2m12s
+NAME                                READY   STATUS             RESTARTS   AGE
+mysql-db-5b85495469-2wnl5           1/1     Running            0          44s
+todo-application-7ff46cfbff-s4tp4   0/1     InvalidImageName   0          31s
+NAME                       TYPE        CLUSTER-IP       EXTERNAL-IP   PORT(S)          AGE
+kubernetes                 ClusterIP   10.96.0.1        <none>        443/TCP          4m55s
+mysql-db-service           ClusterIP   None             <none>        3306/TCP         44s
+todo-application-service   NodePort    10.110.183.189   <none>        8081:30080/TCP   31s
+labuser@ip-172-31-2-198:~/Desktop/todo-application$ 
