9e3d0798b8b9f0da1fd26b9ef2253535ef3316e0
Arun Selvan <53175578+gjarunselvan@users.noreply.github.com>
2026-06-18 13:04:59 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,16 +1,21 @@
-kubectl get nodes
-kubectl get secret
+kubectl delete deployment todo-application
+kubectl apply -f todo-application-deployment.yaml
+
+Then:
+
+kubectl get pods -w
+
+Expected:
+
+todo-application-xxxxx   1/1   Running
+Then verify
 kubectl get pods
 kubectl get svc
-NAME       STATUS   ROLES           AGE     VERSION
-minikube   Ready    control-plane   4m56s   v1.32.0
-NAME                 TYPE                             DATA   AGE
-my-registry-secret   kubernetes.io/dockerconfigjson   1      2m12s
-NAME                                READY   STATUS             RESTARTS   AGE
-mysql-db-5b85495469-2wnl5           1/1     Running            0          44s
-todo-application-7ff46cfbff-s4tp4   0/1     InvalidImageName   0          31s
-NAME                       TYPE        CLUSTER-IP       EXTERNAL-IP   PORT(S)          AGE
-kubernetes                 ClusterIP   10.96.0.1        <none>        443/TCP          4m55s
-mysql-db-service           ClusterIP   None             <none>        3306/TCP         44s
-todo-application-service   NodePort    10.110.183.189   <none>        8081:30080/TCP   31s
-labuser@ip-172-31-2-198:~/Desktop/todo-application$ 
+
+and test:
+
+minikube service todo-application-service --url
+
+or:
+
+curl http://$(minikube ip):30080
