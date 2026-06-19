02c2b832cb738a7ed31fd16bae5b8ccfb58c5f50
Arun Selvan <53175578+gjarunselvan@users.noreply.github.com>
2026-06-19 10:37:57 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
 Amazon ECR plugin
 Failed to load: Amazon ECR plugin (amazon-ecr 1.161.v1a_1e8df852d6)
  - Failed to load: AWS Credentials Plugin (aws-credentials 262.v0efb_28f71f94)
+Docker API Plugin
+Failed to load: Docker API Plugin (docker-java-api 3.7.1-136.v5d70f77a_c3d6)
+ - Plugin is missing: commons-compress-api (1.26.1-2)
+ - Plugin is missing: bouncycastle-api (2.30.1.80-261.v00c0e2618ec3)
+
+Docker plugin
+Failed to load: Docker plugin (docker-plugin 1316.v75635a_002b_0a_)
+ - Failed to load: Instance Identity (instance-identity 203.v15e81a_1b_7a_38)
