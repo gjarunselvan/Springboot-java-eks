055f40536bae167362bdedb129fa14de9ac20ca8
Arun Selvan <53175578+gjarunselvan@users.noreply.github.com>
2026-06-19 16:28:04 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,3 @@
-aws ecr create-repository \
---repository-name springboot-jenkins-ecr-repo \
+aws ecr describe-repositories \
+--repository-names springboot-jenkins-ecr-repo \
 --region us-east-2
-
-aws: [ERROR]: An error occurred (RepositoryAlreadyExistsException) when calling the CreateRepository operation: The repository with name 'springboot-jenkins-ecr-repo' already exists in the registry with id '761018881666'
-ubuntu@ip-172-31-39-80:~$ 
-
