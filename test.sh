e2ef3b2da0132e8c07df854a4ab109c653304970
Arun Selvan <53175578+gjarunselvan@users.noreply.github.com>
2026-06-19 15:33:56 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,66 +1,42 @@
-./set.sh 
-======================================
-FIX JENKINS REPOSITORY
-======================================
-deb [signed-by=/usr/share/keyrings/jenkins-keyring.gpg] https://pkg.jenkins.io/debian-stable binary/
-======================================
-UPDATE PACKAGES
-======================================
-Hit:1 http://us-east-2.ec2.archive.ubuntu.com/ubuntu resolute InRelease
-Hit:2 http://us-east-2.ec2.archive.ubuntu.com/ubuntu resolute-updates InRelease                       
-Hit:3 http://us-east-2.ec2.archive.ubuntu.com/ubuntu resolute-backports InRelease                     
-Ign:4 https://pkg.jenkins.io/debian-stable binary/ InRelease                                          
-Get:5 https://pkg.jenkins.io/debian-stable binary/ Release [2044 B]   
-Get:6 https://pkg.jenkins.io/debian-stable binary/ Release.gpg [833 B]
-Hit:7 http://security.ubuntu.com/ubuntu resolute-security InRelease
-Ign:6 https://pkg.jenkins.io/debian-stable binary/ Release.gpg
-Warning: OpenPGP signature verification failed: https://pkg.jenkins.io/debian-stable binary/ Release: The following signatures couldn't be verified because the public key is not available: NO_PUBKEY 7198F4B714ABFC68
-Error: The repository 'https://pkg.jenkins.io/debian-stable binary/ Release' is not signed.
-Notice: Updating from such a repository can't be done securely, and is therefore disabled by default.
-Notice: See apt-secure(8) manpage for repository creation and user configuration details.
-======================================
-INSTALL JENKINS
-======================================
-Package jenkins is not available, but is referred to by another package.
-This may mean that the package is missing, has been obsoleted, or
-is only available from another source
+#!/bin/bash
 
-Error: Package 'jenkins' has no installation candidate
-Failed to enable unit: Unit jenkins.service does not exist
-Failed to start jenkins.service: Unit jenkins.service not found.
-======================================
-FIX DOCKER PERMISSIONS
-======================================
-======================================
-CREATE DOCKER VOLUME
-======================================
-jenkins_data
-======================================
-CREATE DOCKER NETWORK
-======================================
-Error response from daemon: network with name jenkins_network already exists
-======================================
-VERIFY SERVICES
-======================================
+echo "======================================"
+echo "INSTALL JENKINS MANUALLY"
+echo "======================================"
 
-JENKINS STATUS
-inactive
+wget -O jenkins.deb https://pkg.jenkins.io/debian-stable/binary/jenkins_2.516.1_all.deb
 
-DOCKER STATUS
-active
+sudo apt update
 
-DOCKER VOLUMES
-DRIVER    VOLUME NAME
-local     jenkins_data
+sudo apt install -y fontconfig openjdk-17-jre
 
-DOCKER NETWORKS
-0f04cffead58   jenkins_network   bridge    local
+sudo dpkg -i jenkins.deb || true
 
-JENKINS PASSWORD
-cat: /var/lib/jenkins/secrets/initialAdminPassword: No such file or directory
+sudo apt --fix-broken install -y
 
-PUBLIC IP
-3.137.153.32
-OPEN JENKINS
-http://3.137.153.32:8080
-ubuntu@ip-172-31-39-80:~$ 
+sudo systemctl enable jenkins
+
+sudo systemctl start jenkins
+
+echo ""
+echo "======================================"
+echo "VERIFY"
+echo "======================================"
+
+echo "Jenkins Status:"
+sudo systemctl is-active jenkins
+
+echo ""
+
+echo "Jenkins Password:"
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+echo ""
+
+echo "Listening Port:"
+sudo ss -tulpn | grep 8080
+
+echo ""
+
+echo "Public URL:"
+echo "http://$(curl -s ifconfig.me):8080"
