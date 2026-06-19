2e7bad860ec52c238a6a02dbc398c0c7b01fada6
Arun Selvan <53175578+gjarunselvan@users.noreply.github.com>
2026-06-19 15:19:23 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,22 +1,75 @@
-ubectl delete deployment todo-application
-deployment.apps "todo-application" deleted
-labuser@ip-172-31-2-198:~/Desktop/todo-application$ kubectl apply -f todo-application-deployment.yaml
-deployment.apps/todo-application created
-service/todo-application-service unchanged
-labuser@ip-172-31-2-198:~/Desktop/todo-application$ kubectl get pods -w
-NAME                                READY   STATUS    RESTARTS   AGE
-mysql-db-5b85495469-2wnl5           1/1     Running   0          4m15s
-todo-application-78c798bdd4-dw2fl   1/1     Running   0          10s
-labuser@ip-172-31-2-198:~/Desktop/todo-application$ kubectl get podsds
-kubectl get svc
-NAME                                READY   STATUS    RESTARTS   AGE
-mysql-db-5b85495469-2wnl5           1/1     Running   0          4m32s
-todo-application-78c798bdd4-dw2fl   1/1     Running   0          27s
-NAME                       TYPE        CLUSTER-IP       EXTERNAL-IP   PORT(S)          AGE
-kubernetes                 ClusterIP   10.96.0.1        <none>        443/TCP          8m43s
-mysql-db-service           ClusterIP   None             <none>        3306/TCP         4m32s
-todo-application-service   NodePort    10.110.183.189   <none>        8081:30080/TCP   4m19s
-labuser@ip-172-31-2-198:~/Desktop/todo-application$ minikube service todo-application-service --url
-http://192.168.49.2:30080
-labuser@ip-172-31-2-198:~/Desktop/todo-application$ ^C
-labuser@ip-172-31-2-198:~/Desktop/todo-application$ 
+Verify:
+
+java -version
+3.3 Install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+sudo apt install unzip -y
+
+unzip awscliv2.zip
+
+sudo ./aws/install
+
+Verify:
+
+aws --version
+3.4 Install AWS IAM Authenticator
+curl -Lo aws-iam-authenticator https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/latest/download/aws-iam-authenticator_linux_amd64
+
+chmod +x aws-iam-authenticator
+
+sudo mv aws-iam-authenticator /usr/local/bin/
+
+Verify:
+
+aws-iam-authenticator version
+3.5 Install Docker
+sudo apt install docker.io -y
+
+Start Docker:
+
+sudo systemctl enable docker
+sudo systemctl start docker
+
+Verify:
+
+docker --version
+sudo systemctl status docker
+3.6 Install Jenkins
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+/usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+/etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update
+
+sudo apt install jenkins -y
+
+Start Jenkins:
+
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+Verify:
+
+sudo systemctl status jenkins
+Get Jenkins Password
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+Access Jenkins
+
+Open:
+
+http://<EC2-PUBLIC-IP>:8080
+
+Paste the password and complete the initial setup.
+
+Before moving to Step 4, verify:
+java -version
+aws --version
+docker --version
+sudo systemctl status docker
+sudo systemctl status jenkins
+
+Send me the outputs (or screenshots), and I'll give you Step 4: Docker Volume + Docker Network + Run Jenkins Container, which is worth 20 marks in your evaluation.
