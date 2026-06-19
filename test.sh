81989f67da33bdc7750d1a25433e73a46ff70ec1
Arun Selvan <53175578+gjarunselvan@users.noreply.github.com>
2026-06-19 16:13:44 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,9 @@
 sudo systemctl stop jenkins
 
-wget -O /tmp/jenkins.war https://get.jenkins.io/war-stable/latest/jenkins.war
-
-sudo cp /tmp/jenkins.war /usr/share/jenkins/jenkins.war
+sudo rm -rf /var/lib/jenkins/plugins/*
 
 sudo systemctl start jenkins
 
 sleep 30
 
-sudo systemctl status jenkins --no-pager
+sudo systemctl status jenkins
