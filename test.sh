ef1119942af923193fb65a59206dcbe41a4cb35a
Arun Selvan <53175578+gjarunselvan@users.noreply.github.com>
2026-06-19 15:53:23 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,28 +1,25 @@
-STEP 14 - Login to Jenkins
+#!/bin/bash
 
-Open:
+echo "Stopping Jenkins..."
+sudo systemctl stop jenkins
 
-http://3.137.153.32:8080
+echo "Backing up old Jenkins data..."
+sudo mv /var/lib/jenkins /var/lib/jenkins_backup_$(date +%s)
 
-Password:
+echo "Creating fresh Jenkins home..."
+sudo mkdir -p /var/lib/jenkins
 
-9169525586d44fc8b5653fa5b72df1d4
-Select
-Install Suggested Plugins
+sudo chown -R jenkins:jenkins /var/lib/jenkins
 
-Wait 5-10 mins.
+echo "Starting Jenkins..."
+sudo systemctl start jenkins
 
-STEP 15 - Create Admin User
+sleep 20
 
-Create exactly:
+echo ""
+echo "New Jenkins Password:"
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 
-Field	Value
-Username	jenkins-admin-user
-Password	jenkins_springboot4684$
-Confirm Password	jenkins_springboot4684$
-Full Name	Jenkins Admin
-Email	your email
-
-Click:
-
-Save and Continue
+echo ""
+echo "Jenkins URL:"
+echo "http://$(curl -s ifconfig.me):8080"
